33ae8490ceb73d896264004706891fded0d8c8c2
Misha Bubnov <bybnov.misha@yandex.ru>
2023-11-10 12:40:36 +0300

    ПР13 испр5

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,9 +1,11 @@
 const Card = require("../models/Card");
-const { BAD_REQUEST_STATUS,
+const {
+  BAD_REQUEST_STATUS,
   SERVER_ERROR_STATUS,
   NOT_FOUND_STATUS,
   CREATED_STATUS,
-  SUCCESS_STATUS} = require("../constants/errorStatus")
+  SUCCESS_STATUS,
+} = require("../constants/errorStatus");
 
 const getCards = async (req, res) => {
   try {
@@ -15,7 +17,9 @@ const getCards = async (req, res) => {
         message: "Переданы некорректные данные.",
       });
     }
-    return res.status(SERVER_ERROR_STATUS).send({ message: "Ошибка на стороне сервера" });
+    return res
+      .status(SERVER_ERROR_STATUS)
+      .send({ message: "Ошибка на стороне сервера" });
   }
 };
 
@@ -31,101 +35,106 @@ const postCard = async (req, res) => {
         message: " Переданы некорректные данные при создании карточки.",
       });
     }
-    return res.status(SERVER_ERROR_STATUS).send({ message: "Ошибка на стороне сервера" });
+    return res
+      .status(SERVER_ERROR_STATUS)
+      .send({ message: "Ошибка на стороне сервера" });
   }
 };
 
 const deleteCard = async (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => {
-      //console.log(card.owner);
-      //console.log(req.user._id);
-      if (!card) {
-        throw new Error("NotFound");
-      }
-      // if (card.owner === req.user._id) {
-      //   res.send(card)
-      // }else {
-      //   return res.status(404).send({message: "Удалить можно только свою карточку!"})
-      // }
-      return res.status(SUCCESS_STATUS).send(card);
-    })
-    .catch((error) => {
-      //console.log(error);
-      if (error.message === "NotFound") {
-        return res
-          .status(NOT_FOUND_STATUS)
-          .send({ message: "Карточка с указанным _id не найдена." });
-      }
-      return res.status(SERVER_ERROR_STATUS).send({ message: "Ошибка на стороне сервера" });
-    });
+  try {
+    if (req.params.cardId.length !== 24) {
+      throw new Error("NotFoundId");
+    }
+
+    const delCard = await Card.findByIdAndRemove(req.params.cardId);
+    console.log(delCard, "тутуут");
+
+    if (!delCard) {
+      throw new Error("NotFound");
+    }
+
+    return res.status(SUCCESS_STATUS).send(delCard);
+  } catch (error) {
+    if (error.message === "NotFoundId") {
+      return res
+        .status(BAD_REQUEST_STATUS)
+        .send({ message: "Передан неправильный _id." });
+    }
+    if (error.message === "NotFound") {
+      return res
+        .status(NOT_FOUND_STATUS)
+        .send({ message: "Карточка с указанным _id не найдена." });
+    }
+    return res
+      .status(SERVER_ERROR_STATUS)
+      .send({ message: "Ошибка на стороне сервера" });
+  }
 };
 
-const likeCard = (req, res) =>
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true }
-  )
-    .then((like) => {
-      if (!req.params.cardId) {
-        //console.log(req.params.cardId);
-        throw new Error("NotFound");
-      }
-      if (!like) {
-        throw new Error("NotFoundDataLike");
-      }
-      return res.status(CREATED_STATUS).send(like);
-    })
-    .catch((error) => {
-      if (error.message === "NotFoundDataLike") {
-        return res
-          .status(BAD_REQUEST_STATUS)
-          .send({
-            message:
-              "Переданы некорректные данные для постановки/снятии лайка.",
-          });
-      }
-      if (error.message === "NotFound") {
-        return res
-          .status(NOT_FOUND_STATUS)
-          .send({ message: "Передан несуществующий _id карточки." });
-      }
-      return res.status(SERVER_ERROR_STATUS).send({ message: "Ошибка на стороне сервера" });
-    });
+const likeCard = (req, res) => {
+  if (req.params.cardId.length === 24) {
+    Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+      { new: true }
+    )
+      .then((like) => {
+        if (!like) {
+          throw new Error("NotFound");
+        }
 
-const dislikeCard = (req, res) =>
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true }
-  )
-    .then((like) => {
-      if (!req.params.cardId) {
-        //console.log(req.params.cardId);
-        throw new Error("NotFound");
-      }
-      if (!like) {
-        throw new Error("NotFoundDataLike");
-      }
-      return res.status(SUCCESS_STATUS).send(like);
-    })
-    .catch((error) => {
-      if (error.message === "NotFoundDataLike") {
+        return res.status(CREATED_STATUS).send(like);
+      })
+      .catch((error) => {
+        if (error.message === "NotFound") {
+          return res
+            .status(NOT_FOUND_STATUS)
+            .send({ message: "Передан несуществующий _id карточки." });
+        }
+
         return res
-          .status(BAD_REQUEST_STATUS)
-          .send({
-            message:
-              "Переданы некорректные данные для постановки/снятии лайка.",
-          });
-      }
-      if (error.message === "NotFound") {
+          .status(SERVER_ERROR_STATUS)
+          .send({ message: "Ошибка на стороне сервера" });
+      });
+  } else {
+    return res
+      .status(BAD_REQUEST_STATUS)
+      .send({ message: "Передан некорректный _id карточки." });
+  }
+};
+
+const dislikeCard = (req, res) => {
+  if (req.params.cardId.length === 24) {
+    Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } }, // убрать _id из массива
+      { new: true }
+    )
+      .then((like) => {
+        if (!like) {
+          throw new Error("NotFound");
+        }
+
+        return res.status(CREATED_STATUS).send(like);
+      })
+      .catch((error) => {
+        if (error.message === "NotFound") {
+          return res
+            .status(NOT_FOUND_STATUS)
+            .send({ message: "Передан несуществующий _id карточки." });
+        }
+
         return res
-          .status(NOT_FOUND_STATUS)
-          .send({ message: "Передан несуществующий _id карточки." });
-      }
-      return res.status(SERVER_ERROR_STATUS).send({ message: "Ошибка на стороне сервера" });
-    });
+          .status(SERVER_ERROR_STATUS)
+          .send({ message: "Ошибка на стороне сервера" });
+      });
+  } else {
+    return res
+      .status(BAD_REQUEST_STATUS)
+      .send({ message: "Передан некорректный _id карточки." });
+  }
+};
 
 module.exports = {
   getCards,
