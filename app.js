8923f4826add8190080726252972ca951db59460
Misha Bubnov <bybnov.misha@yandex.ru>
2023-11-22 21:04:41 +0300

    ПР14 оптимизация app.js

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const router = require('./routes');
 
 const app = express();
 
-const { loginUser, createUser } = require('./controllers/users');
-const { authenticateValidator, createValidator } = require('./middlewares/customValidator');
 const { FoundError } = require('./middlewares/foundError');
+const { errorHandler } = require('./middlewares/error-handler');
 
 const { PORT = 3000 } = process.env;
 
@@ -22,27 +21,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(router);
 
-app.post('/signin', authenticateValidator, loginUser);
-app.post('/signup', createValidator, createUser);
-
 app.use(errors());
 
 app.use(() => {
   throw new FoundError('Страница не найдена', 404);
 });
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const { auth } = require('../middlewares/auth');
+const { authenticateValidator, createValidator } = require('../middlewares/customValidator');
+const { createUser, loginUser } = require('../controllers/users');
 
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
+router.post('/signin', authenticateValidator, loginUser);
+router.post('/signup', createValidator, createUser);
 
 module.exports = router;
